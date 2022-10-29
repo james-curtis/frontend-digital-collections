a6cfdda6845109ad6d1ceaccecb412dea6566348
JamesCurtis <jamescurtis@foxmail.com>
2022-10-30 00:24:44 +0800

    chore: 使用生产环境地址

--- app.config.js
+++ app.config.js
@@ -4,7 +4,8 @@ if (process.env.NODE_ENV === 'development') {
 	baseUrl = 'http://nft1.testsite.tianhaoltd.top/api/'
 } else {
 	//线上路径
-	baseUrl = 'http://api.alpha.jzjxxy.top/api/'
+	// baseUrl = 'http://api.alpha.jzjxxy.top/api/'
+	baseUrl = 'http://api.space-9.cn/api/'
 }
 
 export {
